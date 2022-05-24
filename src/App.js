51599c7ba88ffdea95bafd660560b7b544354548
afirdaus04 <afirdaus.pe@gmail.com>
2022-05-24 14:08:37 +0800

    components folder additions

--- src/App.js
+++ src/App.js
@@ -31,10 +31,43 @@ const App = () => {
         <div className={
           `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}` //(`` noted as template string, activeMenu ? method for true or false statement which is divided by the double dot :)
         }>
-          <div className=""> 
-
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full "> 
+            Navbar
           </div>
         </div>
+
+        <div>
+          <Routes>
+
+            //shortcut CTRL + / for comment ALT+SHIFT+DOWN to copy line ALT+UP/Down to move line to a direction
+            {/* Dashboard */}
+            <Route path="/" element="ECommerce" />
+            <Route path="/ecommerce" element="ECommerce" />
+            
+            {/* Pages */}
+            <Route path="/orders" element="Orders" />
+            <Route path="/employees" element="Employees" />
+            <Route path="/customers" element="Customers" />
+
+            {/* Apps */}
+            <Route path="/kanban" element="Kanban" />
+            <Route path="/editor" element="Editor" />
+            <Route path="/calendar" element="Calendar" />
+            <Route path="/color-picker" element="ColorPicker" />
+            
+            {/* Charts */}
+            <Route path="/line" element="Line" />
+            <Route path="/area" element="Area" />
+            <Route path="/bar" element="Bar" />
+            <Route path="/pie" element="Pie" />
+            <Route path="/financial" element="Financial" />
+            <Route path="/color-maping" element="ColorMapping" />
+            <Route path="/pyramid" element="Pyramid" />
+            <Route path="/stacked" element="Stacked" />
+
+          </Routes>
+        </div>
+
       </div>
     </BrowserRouter>
   </div>
