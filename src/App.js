0430b656b31c6dc70a3da76b5400a6fef57894b4
afirdaus04 <afirdaus.pe@gmail.com>
2022-05-31 18:24:58 +0800

    update sidebar

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial,ColorPicker, ColorMapping, Editor } from './pages';
 
 
+// import context API
+import { useStateContext } from './contexts/ContextProvider';
+
 // import from project file
 import './App.css'
 
 // App creation begins
 const App = () => {
-  const activeMenu = true;
-  
+  const { activeMenu } = useStateContext(); // called as hook
+ 
   return (
   <div>
     <BrowserRouter>
