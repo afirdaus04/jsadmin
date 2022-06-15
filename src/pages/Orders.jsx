e85e907f9cf2d24f22a14604285983ad7947d9d9
afirdaus04 <afirdaus.pe@gmail.com>
2022-06-15 14:25:55 +0800

    Employees Update

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcepExport, PdfExport, Edit, Inject, ExcelExport} from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
 import {ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
